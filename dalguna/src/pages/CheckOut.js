4c46e19df788a8370909cddb3b45ac1613aff317
Hye Soo Park <hyehye@kaist.ac.kr>
2021-11-27 19:33:54 +0900

    checkour 페이지 거의 완성
    - ordernow 눌렀을때  db 업로딩만 제대로 되면 됨

--- dalguna/src/pages/CheckOut.js
+++ dalguna/src/pages/CheckOut.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import RoomCard from "../components/RoomCard";
+import LongButton from "../components/LongButton";
 
 import staticDB from "../db/static.json";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { collection, onSnapshot, doc } from "firebase/firestore";
 import { db } from "../firebase-config.js";
 
 import '../checkout.css'
+import { AiOutlineArrowLeft } from 'react-icons/ai';
 
 function CheckOut() {
 
@@ -18,18 +20,27 @@ function CheckOut() {
     //     raised: 23000,
     //     minOrd: 20000,
     //   }])
-
+    const navigate = useNavigate();
   const { userId, roomId } = useParams()
 
   const [menuItemInfo, setMenuItemInfo] = useState([]) // arr
 
-  const [roomInfo, setRoomInfo] = useState(); // obj from the docID
+  const [roomInfo, setRoomInfo] = useState({}); // obj from the docID
 
   const [checkOutList, setCheckOutList] = useState(<></>)
 
-  const [restInfo, setRestInfo] = useState(); // obj from the static db
+//   const [restInfo, setRestInfo] = useState(); // obj from the static db
 
   const [roomCard, setRoomCard] = useState(<></>)
+
+  const [deliInfo, setDeliInfo] = useState({time:"0~0", addr:" " , expDeliTime:" ", deliFee:0, orgPrice:""})
+
+  const [totPrice, setTotPrice] = useState(0);
+
+  const [orderNowButton, setOrderNowButton] = useState(<LongButton type="secondary">지금 시키고 싶어요!</LongButton>)
+
+  const [orderNow, setOrderNow] = useState(<><span className = "checkout_orderNow-explain">최소 주문금액을 아직 넘지 못했어요</span>
+  {orderNowButton}</>)
 
 
   function getRooms() {
@@ -53,10 +64,21 @@ function CheckOut() {
       setRoomInfo(roomInfoObj);
       console.log(roomInfoObj)
 
-      setRoomCard(<div>
-            <a href="#"> <RoomCard roomInfo={roomInfoObj} photo={false}></RoomCard></a>
+      setRoomCard(<div className="checkOut__roomCard">
+            <RoomCard roomInfo={roomInfoObj} photo={false}></RoomCard>
         </div>)
       setMenuItemInfo(roomInfoObj.parti.filter((el) => String(el.id) === String(userId))[0].menu)
+      
+      const timeNow = new Date()
+      var timeDeli = new Date() 
+      timeDeli.setMinutes(timeNow.getMinutes() + Number(roomInfoObj['rest'].deliInfo.time.split('~')[0]))
+      setDeliInfo({
+          time: roomInfoObj['rest'].deliInfo.time,
+          addr: roomInfoObj.deliInfo.addr,
+          expDeliTime: `${timeDeli.getHours()}:${timeDeli.getMinutes()}`,
+          deliFee: roomInfoObj['rest'].deliInfo.fee / roomInfoObj.parti.length,
+          orgFee: roomInfoObj.parti.length !== 1 ? `${roomInfoObj['rest'].deliInfo.fee}` : "",
+        })
       
     })
   }
@@ -65,42 +87,94 @@ function CheckOut() {
     getRooms();
   }, []);
 
+
+  useEffect(() => {
+      if (roomInfo['parti'] !== undefined) {
+          const poolMon = roomInfo.parti.reduce((prev, curr) => prev + curr.price, 0)
+          
+        
+          if (roomInfo['rest'].deliInfo.minOrder < poolMon) {
+            const numOrdNowPeople = roomInfo.parti.filter((el) => el.ordNow).length
+            if (roomInfo['parti'].filter((el) => String(el.id) === String(userId))[0].ordNow) {
+                setOrderNow(<><span className = "checkout_orderNow-explain">{numOrdNowPeople}명의 사람이 지금 시키고 싶어해요</span>
+                <LongButton type="secondary">지금 시키고 싶어요!</LongButton>
+                <span className = "checkout_orderNow-explain">버튼을 이미 눌렀어요</span></>)
+            } else {
+                setOrderNowButton(<LongButton onClick={plzOrderNow} type="primary">지금 시키고 싶어요!</LongButton>)
+                setOrderNow(<><span className = "checkout_orderNow-explain">{numOrdNowPeople}명의 사람이 지금 시키고 싶어해요</span>
+                {orderNowButton}</>)
+            }
+          }
+      }
+    
+  }, [roomInfo, totPrice])
   
   useEffect(() => {
-      setCheckOutList(menuItemInfo.map((menu) => 
-            <li key={menu.name} style={{listStyle:'none'}} className = "checkout_list_block">
-            <span className="checkout_menu_name">{menu.name}</span>
-            <span className="checkout_menu_price">{menu.price}</span>
-        </li>))
-    
-  }, [menuItemInfo])
+      setCheckOutList(menuItemInfo.map((menu, i) => 
+            <li key={i} style={{listStyle:'none'}} className = "checkout_list_block">
+                <span className="checkout_menu_name">{menu.name} x {menu.qnty}</span>
+                <span className="checkout_menu_price">{new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' })
+                            .format(menu.price * menu.qnty)}</span>
+            </li>))
+
+    const totalPrice = [...menuItemInfo].reduce((prev, curr) => {
+        console.log(curr.price)
+        return(prev + curr.price * curr.qnty)
+     } , deliInfo.deliFee)
+    setTotPrice(totalPrice)
+  }, [menuItemInfo, deliInfo])
     //   const room = roomInfo.map((room) =>
     //       <a href="#"> <RoomCard roomInfo={room}></RoomCard></a>)
+    
 
-
-
-      
+  function plzOrderNow() {
+      console.log(userId)
+      const numOrdNowPeople = roomInfo.parti.filter((el) => el.ordNow).length
+      // 여기에서 디비 업로드
+      setOrderNow(<><span className = "checkout_orderNow-explain">{numOrdNowPeople + 1}명의 사람이 지금 시키고 싶어해요</span>
+            <LongButton type="secondary">지금 시키고 싶어요!</LongButton>
+            <span className = "checkout_orderNow-explain">버튼을 이미 눌렀어요</span></>)
+  }
     return (
         <div className="ui-container">
             <div className ="checkout_title__">
-                Room #2
+                <div className="checkout_title__goBack" onClick={() => navigate(-1)}><AiOutlineArrowLeft/></div>
+                Your Room
             </div>
             {roomCard}
-        <div className="checkout_info_block">
-            <div className="checkout_info_heading">
-                <span className="estimated_time_arrival">ETA 18:32 PM</span>
-                <span className="pickup_location">Pick up location</span>
+            <div className="checkout_info_block">
+                <div className="checkout_info">
+                    <div className="checkout_info-eta">
+                        <span className="estimated_time_arrival checkout_info_heading">{deliInfo.expDeliTime} 도착 예정</span>
+                        <span className="estimated_time_arrival checkout_info_content">{deliInfo.time} mins</span>
+                    </div>
+                </div>
+                <br/>
+                <div className="checkout_info-location">
+                    <span className="pickup_location checkout_info_heading">Pick up location</span>
+                    <span className="pickup_location checkout_info_content">{deliInfo.addr}</span>
+                </div>
             </div>
             <br/>
-            <div className="checkout_info_content">
-                <span className="estimated_time_arrival">50 mins</span>
-                <span className="pickup_location">Areum Hall</span>
+            <ul className="checkOut__orderList">
+                {checkOutList}
+                <li className="checkOut__deliFee">
+                    <span className="checkout_menu_name">배달비</span>
+                    <span className="checkout_menu_price">
+                        <span className="checkout_menu_orgPrice">{deliInfo.orgFee}</span> 
+                        <span style={{color:"rgba(0, 0, 0, 0.2)"}}>➡</span> 
+                        {new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' })
+                            .format(deliInfo.deliFee)} </span>
+                </li>
+                <li className = "checkout_list_block">
+                    <span className="checkout_menu_name" style={{fontWeight:"bold"}}>전체</span>
+                    <span className="checkout_menu_price">{new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' })
+                            .format(totPrice)}</span>
+                </li>
+            </ul>
+            <div className = "checkout_orderNow">
+                {orderNow}
             </div>
-        </div>
-        <br/>
-        <div>
-            {checkOutList}
-        </div>
         </div>
     )
 }
